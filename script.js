9a7d02046dbc14a4fadbd9a6f970ea565f0fbc77
adinyy <cnapmon@gmail.com>
2024-08-25 15:46:36 +0530

    bunch of changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const logos = ['logo1.png', 'logo2.png', 'logo3.png']; // Replace with your logo filenames
+/*const logos = ['logo1.png', 'logo2.png', 'logo3.png']; // Replace with your logo filenames
     let currentLogoIndex = 0;
     const logoContainer = document.getElementById('logo-container');
 
@@ -10,3 +10,4 @@ const logos = ['logo1.png', 'logo2.png', 'logo3.png']; // Replace with your logo
 
     // Change logo every 3 seconds (adjust as needed)
     setInterval(changeLogo, 2000);
+*/
